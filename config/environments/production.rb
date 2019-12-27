5898bb274164e669d33feb47ad84fbf42865ae23
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2019-12-27 00:41:42 -0800

    Revert "Set secret key for heroku app deployment"
    This reverts commit 0da834f0113d827f56d0224b4e879cb478e6bc37.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,8 +1,4 @@
 Rails.application.configure do
-
-  # fix missing 'secret_key_base' for 'production' environment error on Heroku deployment
-  config.secret_key_base = ENV["SECRET_KEY_BASE"]
-
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
